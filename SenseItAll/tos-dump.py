050b89f4e6291237480e08a287adb2bf2d932059
Joseph Anthony C. Hermocilla <jachermocilla@gmail.com>
2017-03-23 12:07:11 +0000

    edits

---
+++ SenseItAll/tos-dump.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+import sys
+import tos
+
+if '-h' in sys.argv:
+    print "Usage:", sys.argv[0], "serial@/dev/ttyUSB0:115200"
+    print "      ", sys.argv[0], "network@host:port"
+    sys.exit()
+
+am = tos.AM()
+
+while True:
+    p = am.read()
+    if p:
+        print p
